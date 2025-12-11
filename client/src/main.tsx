bd5593e419f2986fdc7b7c5495d0c259a796d12c
JanHakanViktor <janhakanv@gmail.com>
2025-12-11 02:42:01 +0100

    Setup initial route and page structure

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { RouterProvider } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import router from "./routes/routes";
 
-createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+const theme = createTheme();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </QueryClientProvider>
+  </StrictMode>
+);
