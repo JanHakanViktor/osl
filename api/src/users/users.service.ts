6b43c543869f542e2b411dca8bf93dfdbae744c3
JanHakanViktor <janhakanv@gmail.com>
2025-12-17 19:20:09 +0100

    Add user creation for service

--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from 'src/users/user.schema';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async createUser(username: string, password: string) {
+    const userExists = await this.userModel.findOne({ username });
+
+    if (userExists) {
+      throw new ConflictException('User Already Exists');
+    }
+    return this.userModel.create({ username, password, isAdmin: false });
+  }
+}
