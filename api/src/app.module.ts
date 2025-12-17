6c022da5a426a4b35918e9135292af79af78708e
JanHakanViktor <janhakanv@gmail.com>
2025-12-17 01:11:50 +0100

    correction

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -13,11 +13,12 @@ import { SessionModule } from 'src/telemetry/session/session.module';
       inject: [ConfigService],
       useFactory: (config: ConfigService) => {
         const uri = config.get<string>('MONGO_URL');
+        const dbName = config.get<string>('MONGO_DB_NAME');
 
         if (!uri) {
           throw new Error('MONGO_URL is not defined');
         }
-        return { uri };
+        return { uri, dbName };
       },
     }),
     TelemetryModule,
