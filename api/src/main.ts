6a7e6cb858e0bd59a2333d0bfad1762fb043a731
JanHakanViktor <janhakanv@gmail.com>
2025-12-19 16:36:14 +0100

    Import NestExpressApplication and trust proxy

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { json } from 'express';
 import cookieSession from 'cookie-session';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.set('trust proxy', 1);
+
   app.use(
     json({ limit: '10mb' }),
     cookieSession({
