98d03a3ccc189b2000e7f556037ffc5aab0dab24
JanHakanViktor <janhakanv@gmail.com>
2025-12-19 05:50:07 +0100

    Loosen Cors, allow methods

--- api/src/main.ts
+++ api/src/main.ts
@@ -20,6 +20,8 @@ async function bootstrap(): Promise<void> {
   app.enableCors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
   await app.listen(3030);
