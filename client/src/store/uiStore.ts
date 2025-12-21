93705dc307f0704ebb923d865ecc6ae85db27d01
JanHakanViktor <janhakanv@gmail.com>
2025-12-21 04:00:57 +0100

    Refactor sign in/sign up to share authDialog instead of creating two

--- client/src/store/uiStore.ts
+++ client/src/store/uiStore.ts
@@ -1,13 +1,24 @@
 import { create } from "zustand";
 
+export type AuthMode = "sign-in" | "sign-up";
+
 type UIState = {
   isOpen: boolean;
+  authMode: AuthMode;
+
   openSignUpDialog: () => void;
-  closeSignUpDialog: () => void;
+  openSignInDialog: () => void;
+  closeAuth: () => void;
+  switchToSignIn: () => void;
+  switchToSignUp: () => void;
 };
 
 export const useUIStore = create<UIState>((set) => ({
   isOpen: false,
-  openSignUpDialog: () => set({ isOpen: true }),
-  closeSignUpDialog: () => set({ isOpen: false }),
+  authMode: "sign-in",
+  closeAuth: () => set({ isOpen: false }),
+  openSignUpDialog: () => set({ isOpen: true, authMode: "sign-up" }),
+  openSignInDialog: () => set({ isOpen: true, authMode: "sign-in" }),
+  switchToSignIn: () => set({ authMode: "sign-in" }),
+  switchToSignUp: () => set({ authMode: "sign-up" }),
 }));
